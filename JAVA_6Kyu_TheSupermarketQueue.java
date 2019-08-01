c0508f9d4a17e3e589f522582baf92e321e14c80
nicesixman <nicesixman@naver.com>
2019-08-01 23:50:36 +0900

    JAVA_6Kyu_TheSupermarketQueue.java
    Eclipse - Launch Operation 설정 하나 때문에 시간을 너무 잡아먹었다.
    오늘은 일단 6Kyu 문제 선정하고 Hello World 띄운 후 마무리.

---
+++ JAVA_6Kyu_TheSupermarketQueue.java
@@ -0,0 +1,8 @@
+package codewars_6Kyu_TheSupermarketQueue;
+
+public class TheSupermarketQueue {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World~");
+    }
+}
