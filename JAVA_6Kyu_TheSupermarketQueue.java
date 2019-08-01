dcd2a9ef5c2b4b750ca830f59d5a561d88d28b06
nicesixman <nicesixman@naver.com>
2019-08-01 23:53:14 +0900

    JAVA_6Kyu_TheSupermarketQueue.java
    Eclipse - Launch Operation 설정 하나 때문에 시간을 너무 잡아먹었다.
    오늘은 일단 6Kyu 문제 선정하고 Hello World 띄운 후 마무리.
    + 해당 소스 코드 Indent Size: 8로 변경.

--- JAVA_6Kyu_TheSupermarketQueue.java
+++ JAVA_6Kyu_TheSupermarketQueue.java
@@ -3,6 +3,6 @@ package codewars_6Kyu_TheSupermarketQueue;
 public class TheSupermarketQueue {
 
     public static void main(String[] args) {
-        System.out.println("Hello World~");
+            System.out.println("Hello World~");
     }
 }
