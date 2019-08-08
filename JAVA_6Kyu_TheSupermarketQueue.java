1593258bbac26b60a8208b1687eb049159beb87c
nicesixman <nicesixman@naver.com>
2019-08-08 22:59:15 +0900

    JAVA_6Kyu_TheSupermarketQueue.java
    https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/java
    수를 입력받기 위한 main() 메소드 작성 완료. (3칸짜리 배열 기준)

--- JAVA_6Kyu_TheSupermarketQueue.java
+++ JAVA_6Kyu_TheSupermarketQueue.java
@@ -1,8 +1,31 @@
 package codewars_6Kyu_TheSupermarketQueue;
 
-public class TheSupermarketQueue {
+import java.io.IOException;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-            System.out.println("Hello World~");
-    }
+public class TheSupermarketQueue {
+	public static void main(String[] args) throws IOException {
+		// Try Sample Test #1
+		int customer[] = new int[3];
+		int queue;
+		
+		// 배열 입력.
+		System.out.println("customer 수를 하나씩 입력하세요.");
+		Scanner customerInput = new Scanner(System.in);
+		for (int i=0; i<3; i++)
+			customer[i] = customerInput.nextInt();
+		
+		// queue 개수 입력.
+		System.out.println("몇 개의 queue가 있는지 입력하세요.");
+		Scanner queueInput = new Scanner(System.in);
+		queue = queueInput.nextInt();
+		
+		queueInput.close();
+		customerInput.close();
+		
+		// debugging
+		for (int i=0; i<3; i++)
+			System.out.println(customer[i]);
+		System.out.println(queue);
+	}
 }
